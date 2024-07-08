92293b1c2d4557062f2d35c7d6dad937130a795e
junho100 <bemodesty306@naver.com>
2024-07-08 16:32:47 +0900

    Add stock decrease logic

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,38 @@
+package com.example.stock.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void descrease() {
+        stockService.decrease(1L, 1L);
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(99L, stock.getQuantity());
+    }
+}
