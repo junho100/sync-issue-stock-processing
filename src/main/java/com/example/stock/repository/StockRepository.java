0b86771074b06ef5c3d03e3289b447ae3ea5174d
junho100 <bemodesty306@naver.com>
2024-07-08 17:35:04 +0900

    prim lock

--- src/main/java/com/example/stock/repository/StockRepository.java
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -1,8 +1,14 @@
 package com.example.stock.repository;
 
+import static jakarta.persistence.LockModeType.PESSIMISTIC_WRITE;
+
 import com.example.stock.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
-
+    @Lock(PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithPessimisticLock(Long id);
 }
