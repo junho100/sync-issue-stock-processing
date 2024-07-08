224930b3654c3ad681e69dd3352f3aa2f1f8f55c
junho100 <bemodesty306@naver.com>
2024-07-08 17:21:48 +0900

    using synchronized keyword

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,5 +36,27 @@ class StockServiceTest {
         stockService.decrease(1L, 1L);
         Stock stock = stockRepository.findById(1L).orElseThrow();
         assertEquals(99L, stock.getQuantity());
+    }
+
+    @Test
+    public void request100sync() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = java.util.concurrent.Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                }finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0L, stock.getQuantity());
     }
 }
